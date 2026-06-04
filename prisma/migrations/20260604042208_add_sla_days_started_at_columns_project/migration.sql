a14d1566492ba5bc075965cb85cfbb6855b946ad
Ihsan Faiz <ihsanfaiz200@gmail.com>
2026-06-04 11:30:28 +0700

    feat: approve and reject button in request detail

---
+++ prisma/migrations/20260604042208_add_sla_days_started_at_columns_project/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE "Project" ADD COLUMN     "slaDays" INTEGER,
+ADD COLUMN     "startedAt" TIMESTAMP(3);
--- src/views/other/request/detail/admin/detail.tsx
+++ src/views/other/request/detail/admin/detail.tsx
@@ -4,9 +4,9 @@ import { useParams } from "next/navigation"
 import { api } from "trpc/react"
 import Link from "next/link"
 import { ArrowLeft } from "iconsax-react"
-import { Typography } from "@mui/material"
+import { Typography, Chip, Button } from "@mui/material"
 import MainCard from "components/MainCard"
-import { CircleCheck, BadgeQuestionMark } from "lucide-react"
+import { CircleCheck, Landmark  } from "lucide-react"
 
 export function DetailView(){
 
@@ -30,34 +30,96 @@ export function DetailView(){
             <div className="flex flex-col lg:grid lg:grid-cols-3 gap-8">
                 <div className="flex flex-col lg:col-span-2 gap-8">
                     <MainCard>
-                        
+                        <div className="flex flex-col gap-3">
+                            <div className="flex items-center justify-between">
+                                <Typography variant="h4">
+                                    {request?.references}
+                                </Typography>
+                                <Chip label={request?.status} color={request?.status === "APPROVED" ? "success" : request?.status === "PENDING" ? "default" : "error" }></Chip>
+                            </div>
+                            <div className="flex gap-3">
+                                <Chip label={request?.priority} color={request?.priority === "MEDIUM" ? "warning" : request?.priority === "LOW" ? "default" : "error" }></Chip>
+                                <Chip label={request?.slaDays + " days"} color="success"></Chip>
+                            </div>
+                            <div className="flex gap-2">
+                                <Chip variant="outlined" label={request?.via} color="primary"></Chip>
+                                <Chip variant="outlined" label={request?.psal} color="primary"></Chip>
+                                <Chip variant="outlined" label={request?.framework} color="primary"></Chip>
+                                <Chip variant="outlined" label={request?.groupType} color="primary"></Chip>
+                                <Chip variant="outlined" label={"v" + request?.version} color="primary"></Chip>
+                                <Chip variant="outlined" label={request?.category} color="primary"></Chip>
+                            </div>
+                        </div>
                     </MainCard>
+                    <MainCard>
+                        <div className="flex flex-col gap-3">
+                            <Typography variant="h4">Feature & Description</Typography>
+                            <Typography sx={{ whiteSpace: 'pre-line' }}>{request?.description}</Typography>
+                        </div>
+                    </MainCard>
+                    {
+                        request?.status !== "APPROVED" ? (
+                            <>
+                            <div className="flex ml-auto gap-3">
+                                <Button variant="shadow" color="error">
+                                    Reject
+                                </Button>
+                                <Button variant="shadow" color="success">
+                                    Approve
+                                </Button>
+                            </div>
+                            </>
+                        ) : (
+                            <>
+                                <div className="flex ml-auto gap-3">
+                                    <Button disabled variant="contained" color="secondary">
+                                        Reject
+                                    </Button>
+                                    <Button disabled variant="contained" color="secondary">
+                                        Approve
+                                    </Button>
+                                </div>
+                            </>
+                        )
+                    }
+                    
                 </div>
                 <div>
                     <MainCard>
-                        <Typography variant="h4" className="flex items-center gap-2">
-                            <BadgeQuestionMark />
-                            Guide
-                        </Typography>
-                        <div className="flex flex-col gap-3 mt-5">
-                            <Typography variant="h6" color="textSecondary" className="flex gap-2 items-center">
-                                <CircleCheck className="size-4"/>
-                                Create a new project.
+                        <Button variant="contained" color="secondary" className="w-full text-start justify-start">
+                            <Typography variant="h4" className="flex items-center gap-5">
+                                <Landmark />
+                                Department {request?.department}
                             </Typography>
-                            <Typography variant="h6" color="textSecondary" className="flex gap-2 items-center">
-                                <CircleCheck className="size-4"/>
-                                Add custom submission fields.
-                            </Typography>
-                            <Typography variant="h6" color="textSecondary" className="flex gap-2 items-center">
-                                <CircleCheck className="size-4"/>
-                                Members complete and submit the required data.
-                            </Typography>
-                            <Typography variant="h6" color="textSecondary" className="flex gap-2 items-center">
-                                <CircleCheck className="size-4"/>
-                                Review all submissions from the admin panel.
-                            </Typography>
+                        </Button>
+                        <div className="flex flex-col gap-3 mt-7">
+                            <div className="flex flex-col">
+                                <Typography variant="h5" color="textSecondary">
+                                    <span>Category</span>
+                                </Typography>
+                                <Typography variant="h5" >
+                                    <span>{request?.category}</span>
+                                </Typography>
+                            </div>
+                            <div className="flex flex-col">
+                                <Typography variant="h5" color="textSecondary">
+                                    <span>Service Type</span>
+                                </Typography>
+                                <Typography variant="h5" >
+                                    <span>{request?.serviceType}</span>
+                                </Typography>
+                            </div>
+                            <div className="flex flex-col">
+                                <Typography variant="h5" color="textSecondary">
+                                    <span>Sub Service Type</span>
+                                </Typography>
+                                <Typography variant="h5" >
+                                    <span>{request?.subServiceType}</span>
+                                </Typography>
+                            </div>
                         </div>
                     </MainCard>
+                    
                 </div>
             </div>
         </>
